fd1a2ba26ed541273d75824fa73b4901ab80b3a5
Sagar Mahajan <66167736+sagarmahajan22@users.noreply.github.com>
2022-10-10 00:09:09 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -8,7 +8,7 @@ int main()
     
     // With recent C/C++ versions, we can also
     // declare an array of user specified size
-   
+   //user vector for dyunamic array
       int arr1[20];
     return 0;
 }
